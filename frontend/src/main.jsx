e2e5bc1aa8d1b14c25eb4e544d971856127bdeae
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-17 19:20:27 +0100

    FRONTEND: DEBUG - BUG FIX

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {
     LanguageProvider
-} from "./context/LanguageContext.jsx";
+} from "./src/context/LanguageContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
