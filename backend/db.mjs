264b4432b9ce575361c9e389ff97c356f1a1d83d
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-17 18:22:55 +0100

    BACKEND: DEBUG - DATABASE BUG FIX

--- backend/db.mjs
+++ backend/db.mjs
@@ -1,10 +1,24 @@
 import Database from "better-sqlite3";
 import path from "path";
 
-// Initialises SQLite database connection using better-sqlite3
-// and loads the local database file from the project directory
+const dbPath = path.join(
+    process.cwd(),
+    "GRADRENT.db"
+);
 
-const db = new Database(path.join(process.cwd(), "GRADRENT.db"));
-console.log("DB PATH WORKING");
+console.log("DB PATH:", dbPath);
+
+const db = new Database(dbPath);
+
+
+console.log(
+    "TABLES:",
+    db.prepare(`
+        SELECT name 
+        FROM sqlite_master 
+        WHERE type='table'
+    `).all()
+);
+
 
 export default db;
