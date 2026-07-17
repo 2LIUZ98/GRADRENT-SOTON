ac58411f7c8f4fd03823e13780cca9ff6d1bf0b4
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-17 18:16:04 +0100

    FRONTEND: DEBUG - BUG FIX

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -424,7 +424,7 @@ export default function Header({
 
                     <Link
 
-                        to="/staff-login"
+                        to="/staff"
 
                         onClick={()=>setOpen(false)}
 
